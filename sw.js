a96d33677c71c86aa7bb1c3efa2929a53a833b99
Vinicius <viniciusbsilva@usp.br>
2023-06-22 23:18:55 -0300

    :rocket: Update site

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230622.120149';
+const cacheName = 'chirpy-20230622.231741';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
